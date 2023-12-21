37cc4c2e72e29513992e3b9abbb15b4e279d9a72
Sameer747 <sameer.mohsin@ideas.com.pk>
2023-12-21 17:41:46 +0500

    now the basic functionality looks ok

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,38 @@
+// ignore_for_file: prefer_const_constructors
+
 import 'dart:async';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_inappwebview/flutter_inappwebview.dart';
+import 'package:permission_handler/permission_handler.dart';
 import 'package:url_launcher/url_launcher.dart';
+
+import 'SplashScreen.dart';
+
+List<String> linksToOpenInChrome = [
+  "mailto:corporate@ideas.com.pk",
+  "https://api.whatsapp.com/send/",
+  "https://www.facebook.com/GulahmedFashion",
+  "https://www.instagram.com/gulahmedfashion/",
+  "https://twitter.com/gulahmedfashion?lang=en",
+  "https://www.youtube.com/channel/UCsAIxl3qvpy1DofpwDEx6Ow",
+  "https://www.tiktok.com/@ideasbygulahmed",
+  "https://web.whatsapp.com/send?text=https://www.gulahmedshop.com/",
+  "https://m.facebook.com/login",
+  "https://twitter.com/intent/tweet",
+  "http://pinterest.com/pin/create/button/"
+];
 
 Future main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
-  if (!kIsWeb && defaultTargetPlatform == TargetPlatform.android || defaultTargetPlatform == TargetPlatform.iOS) {
+  if (!kIsWeb && defaultTargetPlatform == TargetPlatform.android ||
+      defaultTargetPlatform == TargetPlatform.iOS) {
     await InAppWebViewController.setWebContentsDebuggingEnabled(kDebugMode);
   }
-
-  runApp(const MaterialApp(debugShowCheckedModeBanner: false, home: MyApp()));
+  await Permission.camera.request();
+  await Permission.storage.request();
+  runApp(MaterialApp(debugShowCheckedModeBanner: false, home: MyApp()));
 }
 
 class MyApp extends StatefulWidget {
@@ -26,15 +47,20 @@ class _MyAppState extends State<MyApp> {
 
   InAppWebViewController? webViewController;
   InAppWebViewSettings settings = InAppWebViewSettings(
+      userAgent:
+          "Mozilla/5.0 (Linux; Android 12) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Mobile Safari/537.36",
+      preferredContentMode: UserPreferredContentMode.MOBILE,
       cacheEnabled: true,
       isInspectable: kDebugMode,
+      javaScriptCanOpenWindowsAutomatically: true,
+      transparentBackground: true,
       mediaPlaybackRequiresUserGesture: false,
       allowsInlineMediaPlayback: true,
-      iframeAllow: "camera; microphone",
+      iframeAllow: "camera;",
       iframeAllowFullscreen: true);
 
   PullToRefreshController? pullToRefreshController;
-  String url = "";
+  String url = "https://www.gulahmedshop.com/";
   double progress = 0;
   final urlController = TextEditingController();
 
@@ -91,8 +117,7 @@ class _MyAppState extends State<MyApp> {
               },
               child: InAppWebView(
                 key: webViewKey,
-                initialUrlRequest:
-                    URLRequest(url: WebUri("https://www.gulahmedshop.com/")),
+                initialUrlRequest: URLRequest(url: WebUri(url)),
                 initialSettings: settings,
                 pullToRefreshController: pullToRefreshController,
                 onWebViewCreated: (controller) {
@@ -109,29 +134,11 @@ class _MyAppState extends State<MyApp> {
                       resources: request.resources,
                       action: PermissionResponseAction.GRANT);
                 },
-                shouldOverrideUrlLoading: (controller, navigationAction) async {
-                  var uri = navigationAction.request.url!;
-
-                  if (![
-                    "http",
-                    "https",
-                    "file",
-                    "chrome",
-                    "data",
-                    "javascript",
-                    "about"
-                  ].contains(uri.scheme)) {
-                    if (await canLaunchUrl(uri)) {
-                      // Launch the App
-                      await launchUrl(
-                        uri,
-                      );
-                      // and cancel the request
-                      return NavigationActionPolicy.CANCEL;
-                    }
-                  }
-
-                  return NavigationActionPolicy.ALLOW;
+                shouldOverrideUrlLoading: _shouldOverrideUrlLoading,
+                onLoadStart: (controller, url) async {
+                  setState(() {
+                    SplashScreen();
+                  });
                 },
                 onLoadStop: (controller, url) async {
                   pullToRefreshController?.endRefreshing();
@@ -152,7 +159,30 @@ class _MyAppState extends State<MyApp> {
                     urlController.text = url;
                   });
                 },
+                onCreateWindow: (controller, createWindowAction) async {
+                  // create a headless WebView using the createWindowAction.windowId to get the correct URL
+                  HeadlessInAppWebView? headlessWebView;
+                  headlessWebView = HeadlessInAppWebView(
+                    windowId: createWindowAction.windowId,
+                    onLoadStart: (controller, url) async {
+                      if (url != null) {
+                        launchUrl(url,
+                            mode: LaunchMode
+                                .externalNonBrowserApplication); // to open with the system browser
+                        // or use the https://pub.dev/packages/url_launcher plugin
+                      }
+                      // dispose it immediately
+                      await headlessWebView?.dispose();
+                      headlessWebView = null;
+                    },
+                  );
+                  headlessWebView?.run();
+
+                  // return true to tell that we are handling the new window creation action
+                  return true;
+                },
                 onUpdateVisitedHistory: (controller, url, androidIsReload) {
+                  print("here is the url: " + url.toString());
                   setState(() {
                     this.url = url.toString();
                     urlController.text = this.url;
@@ -166,7 +196,10 @@ class _MyAppState extends State<MyApp> {
               ),
             ),
             progress < 1.0
-                ? LinearProgressIndicator(value: progress)
+                ? LinearProgressIndicator(
+                    backgroundColor: Colors.white,
+                    color: Colors.green,
+                    value: progress)
                 : Container(),
           ],
         ),
@@ -195,5 +228,68 @@ class _MyAppState extends State<MyApp> {
       //   ],
       // ),
     ])));
+  }
+}
+
+Future<bool> _launchURL(String uri) async {
+  return await launchUrl(
+    uri as Uri,
+    mode: LaunchMode.platformDefault,
+  );
+}
+
+Future<NavigationActionPolicy> _shouldOverrideUrlLoading(
+    InAppWebViewController controller,
+    NavigationAction shouldOverrideUrlLoadingRequest) async {
+  var uri = shouldOverrideUrlLoadingRequest.request.url;
+  if (uri == null) {
+    return NavigationActionPolicy.CANCEL;
+  }
+
+  final uriString = uri.toString();
+  print("sameer");
+  for (var element in linksToOpenInChrome) {
+    print("url :" + uriString);
+    print("List: " + element);
+  }
+  if (uriString.startsWith("whatsapp://send") ||
+      uriString.startsWith("https://m.facebook.com/login") ||
+      uriString.startsWith("https://twitter.com/intent/tweet") ||
+      uriString.startsWith(" http://pinterest.com/pin")) {
+    if (await canLaunchUrl(uri)) {
+      await launchUrl(uri, mode: LaunchMode.externalApplication);
+    } else {
+      launchUrl(uri, mode: LaunchMode.externalNonBrowserApplication);
+    }
+    return NavigationActionPolicy.CANCEL;
+  }
+  if (linksToOpenInChrome.contains(uriString)) {
+    print("links to open in chrome");
+    await launchUrl(uri, mode: LaunchMode.externalApplication);
+    return NavigationActionPolicy.CANCEL;
+  } else if (uriString.contains("https://www.gulahmedshop.com/")) {
+    print("links to open in app: " + uriString);
+    return NavigationActionPolicy.ALLOW;
+  } else {
+    // Handle other schemes as needed
+    await launchUrl(uri, mode: LaunchMode.externalApplication);
+    return NavigationActionPolicy.CANCEL;
+  }
+}
+
+Future<bool> _onCreateWindow(
+    InAppWebViewController controller, CreateWindowAction action) async {
+  var uri = action.request.url;
+  if (uri == null) {
+    // controller.goBack();
+    return false;
+  }
+  final uriString = uri.toString();
+  if (uriString.startsWith('http://') || uriString.startsWith('https://')) {
+    return true;
+  } else {
+    // controller.goBack();
+    _launchURL(uriString);
+    return false;
   }
 }
